d36d9426617d6cd436f75b752a9d4ba2515b6f99
xferrin <xaime.ferrin@gmail.com>
2026-06-10 22:43:26 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import argparse
 from exercises import ex1
+from exercises import ex2
+from exercises import ex3
+
 
 def run_ex1():
     data = ex1.load_and_eda("data/LaLiga_Matches.csv")
     ex1.plot_home_away_goals(data)
     print("Ejercicio 1 completado.")
-    from exercises import ex2
+
 
 def run_ex2():
     data = ex1.load_and_eda("data/LaLiga_Matches.csv")
@@ -15,6 +18,14 @@ def run_ex2():
     print("Ejercicio 2 completado.")
 
 
+def run_ex3():
+    data = ex1.load_and_eda("data/LaLiga_Matches.csv")
+    stats = ex3.compute_team_stats(data)
+    print(stats)
+    ex3.plot_team_wins(stats)
+    print("Ejercicio 3 completado.")
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-ex", type=int, required=True, help="Ejercicio a ejecutar (1-7)")
@@ -22,15 +33,14 @@ def main():
 
     if args.ex == 1:
         run_ex1()
+    elif args.ex == 2:
+        run_ex2()
+    elif args.ex == 3:
+        run_ex3()
     else:
         print("Ejercicio no implementado todavía.")
 
+
 if __name__ == "__main__":
     main()
-if args.ex == 1:
-    run_ex1()
-elif args.ex == 2:
-    run_ex2()
-else:
-    print("Ejercicio no implementado todavía.")
 
